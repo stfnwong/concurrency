a0e621df4762f5a7e4a8036272633e82f9286543
Stefan Wong <stfnwong@gmail.com>
2018-02-19 17:27:46 +1300

    Changed list_mutex implementation to be object-oriented

--- src/list_mutex.cpp
+++ src/list_mutex.cpp
@@ -5,31 +5,61 @@
 #include <list>
 #include <mutex>
 #include <algorithm>
+#include <string>
 
-// globals here for sake of example 
-std::list<int> some_list;
-std::mutex some_mutex;
-
-void add_to_list(int new_value)
+class SomeData
 {
-    std::lock_guard<std::mutex> guard(some_mutex);
-    some_list.push_back(new_value);
-    // note: when do we unlock the mutex?
+    int a;
+    std::string b;
+
+    public:
+        void do_something(void);
+};
+
+void SomeData::do_something(void)
+{
+    for(unsigned n = 0; n < 8; n++)
+        fprintf(stdout, "do_something cycle %d\n", n);
 }
 
-bool list_contains(int value_to_find)
+class DataWrapper
 {
-    std::lock_guard <std::mutex> guard(some_mutex);
-    return std::find(some_list.begin(), some_list.end(), value_to_find) != some_list.end();
+    private:
+        SomeData data;
+        std::mutex mutex;
+    public:
+        template <typename F> void process_data(F func)
+        {
+            std::lock_guard <std::mutex> lock(this->mutex);
+            func(data);
+        }
+};
+
+// instantiate some badly constrained data 
+SomeData *unprotected_data;
+
+void malicious_function(SomeData &protected_data)
+{
+    unprotected_data = &protected_data;
 }
+
+DataWrapper X;
+
+void foo(void)
+{
+    X.process_data(malicious_function); // pass in something dangerous
+    unprotected_data->do_something();         // unprotected access to protected data 
+}
+
 
 int main(int argc, char *argv[])
 {
-    for(unsigned n = 6; n < 32; n++)
-        add_to_list(n);
+    foo();
+    //for(unsigned n = 6; n < 32; n++)
+    //    add_to_list(n);
 
-    for(unsigned n = 0; n < 32; n++)
-        fprintf(stdout, "List %s contain %d\n", list_contains(n) ? "does" : "does not", n);
+    //for(unsigned n = 0; n < 32; n++)
+    //    fprintf(stdout, "List %s contain %d\n", list_contains(n) ? "does" : "does not", n);
 
     return 0;
 }
